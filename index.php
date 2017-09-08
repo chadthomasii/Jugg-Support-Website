ab7f5c8c59515aa00b4457fc6bb227e215406830
Chad <chad.thomas.ii@gmail.com>
2017-09-08 17:50:35 -0400

    Completed Twitter Feed section, Contact section, and footer.

--- index.php
+++ index.php
@@ -80,12 +80,68 @@
             </div>
             
         </div>
-        
-        <div class="social-container">
 
+        <div class="social">
+            <div class="social-heading">
+                <h1>Follow @JuggApp for News and Updates!</h1>
+            </div>
+            <div class="social-container">
+                <a class="twitter-timeline" data-width="600" data-height="500" href="https://twitter.com/JuggApp">Tweets by JuggApp</a> 
+                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
         </div>
       
-        
+        <div class="contact">
+            <div class="contact-heading">
+                <h1>Have a question? Get in Touch!</h1>
+            </div>
+            <div class="contact-container">
+                <div class="form">
+                    <form method="post" action="#">
+                    
+                        <div class="form-name">
+                            <input type="text" placeholder="Enter Your Name" name="name" id="name" required>
+                        </div>
+
+                        <div class="form-email">
+                            <input type="email" placeholder="Enter Your Email" name="email" id="email" required>
+                        </div>
+
+                        <div class="form-phone">
+                            <input type="tel" placeholder="Enter Your Phone Number" name="phone" id="phone">
+                        </div>
+
+                        <div class="form-company">
+                            <input type="text" placeholder="Enter Your Company" name="company" id="company">
+                        </div>
+
+                        <div class="form-comment">
+                            <textarea name="comment" required></textarea>
+                        </div>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
+        <footer>
+            <div class="footer-social">
+                <ul>
+                    <a href="https://instagram.com/juggapp" target="_blank"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/juggapp" target="_blank"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
+                    <a href="https://facebook.com/juggapp" target="_blank"><i class="fa fa-facebook fa-3x" aria-hidden="true"></i></a>
+                </ul>
+            </div>
+
+            <div class="footer-content">
+                <h1>&copy; <?php echo date("Y"); ?> Jugg App</h1>
+                <h1><a href="privacy.pdf" target="_blank">Privacy Policy</a></h1>
+                <h1><a href="JuggTerms.pdf" target="_blank">Terms and Conditions</a></h1>
+                <h1><a href="disclaimer.pdf" target="_blank">Disclaimer</a></h1>
+                <h1><a href="https://icons8.com" target="_blank">Extras</a></h1>
+            </div>
+        </footer>
        
         
     </body>
